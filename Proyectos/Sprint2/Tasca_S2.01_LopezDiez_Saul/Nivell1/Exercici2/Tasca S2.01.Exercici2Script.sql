c4bbc908bccfd24b9b93fe2f1e6d72369a88d02a
Slopezdiez <slopezdiez@gmail.com>
2022-07-25 18:12:45 +0200

    Nivell 1 Exercici2

---
+++ Proyectos/Sprint2/Tasca_S2.01_LopezDiez_Saul/Nivell1/Exercici2/Tasca S2.01.Exercici2Script.sql
@@ -0,0 +1,190 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pizzeria
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `botiga`
+--
+
+DROP TABLE IF EXISTS `botiga`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `botiga` (
+  `idbotiga` int NOT NULL AUTO_INCREMENT,
+  `adreca` varchar(45) DEFAULT NULL,
+  `codi postal` varchar(45) DEFAULT NULL,
+  `localitat` varchar(45) DEFAULT NULL,
+  `provincia` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idbotiga`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `botiga`
+--
+
+LOCK TABLES `botiga` WRITE;
+/*!40000 ALTER TABLE `botiga` DISABLE KEYS */;
+/*!40000 ALTER TABLE `botiga` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `idcategoria` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client` (
+  `idclient` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) DEFAULT NULL,
+  `cognoms` varchar(45) DEFAULT NULL,
+  `adreca` varchar(45) DEFAULT NULL,
+  `codi postal` varchar(45) DEFAULT NULL,
+  `localitat` varchar(45) DEFAULT NULL,
+  `provincia` varchar(45) DEFAULT NULL,
+  `telefon` int DEFAULT NULL,
+  PRIMARY KEY (`idclient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comanda`
+--
+
+DROP TABLE IF EXISTS `comanda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comanda` (
+  `idcomanda` int NOT NULL AUTO_INCREMENT,
+  `data comanda` datetime DEFAULT NULL,
+  `tipus_comanda` varchar(45) DEFAULT NULL,
+  `quantitat_pizzes` int DEFAULT NULL,
+  `quantitat_hamburgueses` int DEFAULT NULL,
+  `quantitat_begudes` int DEFAULT NULL,
+  `preu_Total` int DEFAULT NULL,
+  `idEmpleat` int DEFAULT NULL,
+  `data lliurament` datetime DEFAULT NULL,
+  PRIMARY KEY (`idcomanda`),
+  KEY `idEmpleat_idx` (`idEmpleat`),
+  CONSTRAINT `idEmpleat` FOREIGN KEY (`idEmpleat`) REFERENCES `empleats` (`idempleats`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comanda`
+--
+
+LOCK TABLES `comanda` WRITE;
+/*!40000 ALTER TABLE `comanda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comanda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleats`
+--
+
+DROP TABLE IF EXISTS `empleats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleats` (
+  `idempleats` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) DEFAULT NULL,
+  `cognoms` varchar(45) DEFAULT NULL,
+  `NIF` varchar(45) DEFAULT NULL,
+  `telefon` varchar(45) DEFAULT NULL,
+  `professio` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idempleats`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleats`
+--
+
+LOCK TABLES `empleats` WRITE;
+/*!40000 ALTER TABLE `empleats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productes`
+--
+
+DROP TABLE IF EXISTS `productes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productes` (
+  `idproductes` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) DEFAULT NULL,
+  `descripcio` varchar(45) DEFAULT NULL,
+  `imatge` varchar(45) DEFAULT NULL,
+  `preu` int DEFAULT NULL,
+  PRIMARY KEY (`idproductes`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productes`
+--
+
+LOCK TABLES `productes` WRITE;
+/*!40000 ALTER TABLE `productes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-25 18:05:04
