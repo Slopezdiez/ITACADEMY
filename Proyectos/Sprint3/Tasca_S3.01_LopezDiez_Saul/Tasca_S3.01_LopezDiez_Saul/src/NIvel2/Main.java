4c480675b749b1df036a87a0a7974723f04e4f04
Slopezdiez <slopezdiez@gmail.com>
2022-08-13 19:35:03 +0200

    Modificacion ultimo commit s3_01 nivel 2

--- Proyectos/Sprint3/Tasca_S3.01_LopezDiez_Saul/Tasca_S3.01_LopezDiez_Saul/src/NIvel2/Main.java
+++ Proyectos/Sprint3/Tasca_S3.01_LopezDiez_Saul/Tasca_S3.01_LopezDiez_Saul/src/NIvel2/Main.java
@@ -1,4 +1,37 @@
 package NIvel2;
 
+import NIvel2.inter.FabricaAbstracta;
+import NIvel2.inter.IDirecciones;
+import NIvel2.inter.ITelefonos;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Agenda agenda = new Agenda();
+        FabricaAbstracta fabricaD = FabricaProductor.getFactory("Direccion");
+        IDirecciones direcciones = fabricaD.getPaisDireccion("España");
+
+        direcciones.añadir();
+        agenda.añadirDirección(direcciones);
+        direcciones = fabricaD.getPaisDireccion("Francia");
+        direcciones.añadir();
+        agenda.añadirDirección(direcciones);
+        direcciones = fabricaD.getPaisDireccion("Portugal");
+        direcciones.añadir();
+        agenda.añadirDirección(direcciones);
+
+
+        FabricaAbstracta fabricaT = FabricaProductor.getFactory("telefono");
+        ITelefonos telefonos = fabricaT.getPaisTelefono("España");
+
+        telefonos.añadir();
+        agenda.añadirTelefono(telefonos);
+        telefonos = fabricaT.getPaisTelefono("Francia");
+        telefonos.añadir();
+        agenda.añadirTelefono(telefonos);
+        telefonos = fabricaT.getPaisTelefono("Portugal");
+        telefonos.añadir();
+        agenda.añadirTelefono(telefonos);
+        System.out.println(agenda);
+    }
 }
