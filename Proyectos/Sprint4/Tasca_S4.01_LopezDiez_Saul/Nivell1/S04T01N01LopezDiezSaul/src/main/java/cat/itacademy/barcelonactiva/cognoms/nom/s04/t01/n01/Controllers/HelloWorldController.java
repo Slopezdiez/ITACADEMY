60df92ee9342cfcf96b07e0218585ba936d7dd47
Slopezdiez <slopezdiez@gmail.com>
2022-11-04 12:05:35 +0100

    Sprint 4 Tasca 1 Nivell 1 Complet

---
+++ Proyectos/Sprint4/Tasca_S4.01_LopezDiez_Saul/Nivell1/S04T01N01LopezDiezSaul/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/Controllers/HelloWorldController.java
@@ -0,0 +1,28 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/saluda")
+    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom) {
+        return String.format("Hola %s!. Estàs executant un projecte Maven", nom);
+    }
+
+
+ @GetMapping(value={"/saluda2", "/saluda2/{nom}"})
+ @ResponseBody
+ public String saluda2(@PathVariable Optional<String> nom) {
+     if (nom.isPresent()) {
+         return String.format("Hola %s!. Estàs executant un projecte Maven", nom.get());
+     } else {
+         return "Falta el parametre nom";
+     }
+
+
+    }
+
+}
--- Proyectos/Sprint4/Tasca_S4.01_LopezDiez_Saul/Nivell1/S04T01N01LopezDiezSaul/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01LopezDiezSaulApplication.java
+++ Proyectos/Sprint4/Tasca_S4.01_LopezDiez_Saul/Nivell1/S04T01N01LopezDiezSaul/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01LopezDiezSaulApplication.java
@@ -2,6 +2,8 @@ package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class S04T01N01LopezDiezSaulApplication {
@@ -9,5 +11,7 @@ public class S04T01N01LopezDiezSaulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(S04T01N01LopezDiezSaulApplication.class, args);
 	}
+
+
 
 }
